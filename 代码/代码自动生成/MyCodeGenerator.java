fd2eb8056f37111277013032181b9059d9729873
chang <1584541746@qq.com>
2020-07-26 22:49:05 +0800

    .

---
+++ 代码/代码自动生成/MyCodeGenerator.java
@@ -0,0 +1,126 @@
+package chang;
+
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+import java.util.ArrayList;
+
+public class MyCodeGenerator {
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        // 项目的目录
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("chang");
+        gc.setOpen(false);
+        // 是否覆盖
+        gc.setFileOverride(false);
+        // 去service的I前缀
+        gc.setServiceName("%sService");
+        gc.setIdType(IdType.ASSIGN_ID);
+        gc.setDateType(DateType.ONLY_DATE);
+        // Swagger2 注解
+        gc.setSwagger2(true);
+        mpg.setGlobalConfig(gc);
+
+        // 设置数据源
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/mybatis-plus?useSSL=true&useUnicode=true&characterEncoding=utf8&serverTimezone=UTC");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        dsc.setDbType(DbType.MYSQL);
+        mpg.setDataSource(dsc);
+
+        // 包的配置
+        PackageConfig pc = new PackageConfig();
+        // pc.setModuleName("mybatis_plus");
+        pc.setParent("chang");
+        pc.setEntity("entity");
+        pc.setMapper("mapper");
+        pc.setService("service");
+        pc.setController("controller");
+        pc.setXml("mapper");
+        mpg.setPackageInfo(pc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        // 要映射的表明
+        strategy.setInclude("user");
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityLombokModel(true);
+        // 逻辑删除
+        strategy.setLogicDeleteFieldName("deleted");
+        // 自动填充
+        TableFill create_time = new TableFill("create_time", FieldFill.INSERT);
+        TableFill update_time = new TableFill("update_time", FieldFill.INSERT_UPDATE);
+        ArrayList<TableFill> tableFills = new ArrayList<>();
+        tableFills.add(update_time);
+        tableFills.add(create_time);
+        strategy.setTableFillList(tableFills);
+        // 乐观锁
+        strategy.setVersionFieldName("version");
+        // Controller驼峰命名
+        strategy.setRestControllerStyle(true);
+        // 请求中有下划线 比如： hello_id_2
+        strategy.setControllerMappingHyphenStyle(true);
+        mpg.setStrategy(strategy);
+
+        mpg.execute();
+
+    }
+
+}
+
+/*
+
+		<dependency>
+			<groupId>com.baomidou</groupId>
+			<artifactId>mybatis-plus-generator</artifactId>
+			<version>3.3.2</version>
+		</dependency>
+		<dependency>
+			<groupId>mysql</groupId>
+			<artifactId>mysql-connector-java</artifactId>
+		</dependency>
+		<dependency>
+			<groupId>org.apache.velocity</groupId>
+			<artifactId>velocity-engine-core</artifactId>
+			<version>2.2</version>
+		</dependency>
+
+		<dependency>
+			<groupId>com.alibaba</groupId>
+			<artifactId>druid</artifactId>
+			<version>1.1.22</version>
+		</dependency>
+
+		<!--swagger2-->
+		<dependency>
+			<groupId>io.springfox</groupId>
+			<artifactId>springfox-swagger2</artifactId>
+			<version>2.9.2</version>
+		</dependency>
+
+		<dependency>
+			<groupId>io.springfox</groupId>
+			<artifactId>springfox-swagger-ui</artifactId>
+			<version>2.9.2</version>
+		</dependency>
+
+ */
